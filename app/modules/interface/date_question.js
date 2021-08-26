1d18d4f0fd5db5d08fb649d2b937b50a0c3bda52
Tony-404 <chernani2003@gmail.com>
2021-08-25 23:46:04 -0500

    creacion del archivo option y estadistica, funcionamiento de las imagenes de optiones con <a>

--- app/modules/interface/date_question.js
+++ app/modules/interface/date_question.js
@@ -8,41 +8,48 @@ function section_seleccion(){
         <h1>Practica tus conocimientos en la categoria que prefieras</h1>
     </div>
     <div id="questions">
+        <a href="">
         <div id="html"></div>
+        </a>
         <label class="has-text-light">HTML</label>
         <div id="items">
             <div id="questions">
-                <div id="css"></div>
+                <a href="">
+                    <div id="css"></div>
+                </a>
                 <label class="has-text-light">CSS</label>
             </div>
             <div id="questions">
-                <div id="js"></div>
+                <a href="">
+                    <div id="js"></div>
+                </a>
                 <label class="has-text-light">JS</label>
             </div>
         </div>
         <div id="items">
             <div id="questions">
-                <div id="figma"></div>
+                <a href=""> 
+                    <div id="figma"></div>
+                </a>
                 <label class="has-text-light">FIGMA</label>
             </div>
             <div id="questions">
-                <div id="ux"></div>
+                <a href="">
+                  <div id="ux"></div>
+                </a>
                 <label class="has-text-light">UX</label>
             </div>
         </div>
         <nav class="navbar" role="navigation" aria-label="main navigation" style="background: #232E35;  height:69px; position:fixed; bottom:0; width:100vw; padding: 12px 16px 12px 16px">
             <div class="navbar-brand columns has-text-centered ">
                 <div class="column items">
-                        <img src="./image/navbar/verde/home.png" alt="">
-                    </a>
+                    <a><img src="./image/navbar/verde/home.png" alt=""></a>
                 </div>
                 <div class="column items">
-                        <img src="./image/navbar/normal/estadistica.png" alt="">
-                    </a>
+                    <img src="./image/navbar/normal/estadistica.png" alt=""></a>
                 </div>
                 <div class="column items">
-                        <img src="./image/navbar/normal/perfil.png" alt="">
-                    </a>
+                    <a><img src="./image/navbar/normal/perfil.png" alt=""></a>
                 </div>
             </div>
         </nav>
